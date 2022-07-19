4f937c255701e56e9cbb6e99d55dfd2a0e1e6ce0
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-07-19 16:12:22 +0530

    Initial setup for parcel and sass libraries

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message}${res.status}`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+fetch(
+  'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+);
+
+showRecipe();
